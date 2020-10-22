9b97e18fef3c13f7dc05d2ed620c2bc2baca86cb
Victor <victorasauceda@gmail.com>
2020-10-22 10:28:50 -0700

    changing images to cosb

--- config/site.js
+++ config/site.js
@@ -6,10 +6,10 @@ module.exports = {
   url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
   siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
-  logo: 'static/logo/logo.png', // Used for SEO
-  banner: 'static/logo/banner.png',
+  logo: 'static/logo/cosb.png', // Used for SEO
+  banner: 'static/logo/cosb.png',
   // JSONLD / Manifest
-  favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
+  favicon: 'static/logo/cosb.png', // Used for manifest favicon generation
   shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
   author: 'Justin', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
