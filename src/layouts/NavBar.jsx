d98f20407738ea60cb71af23023bf2c85910bfa2
Victor <victorasauceda@gmail.com>
2020-10-22 16:58:47 -0700

    style

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -28,7 +28,7 @@ const Nav = styled.nav`
 `;
 
 const NavBar = () => (
-  <Headroom calcHeightOnResize>
+  <Headroom style={{ marginBottom: '2rem' }}>
     <StyledLink to="/">
       <img style={{ width: '10rem' }} src={logo} alt="Santa Barbara Logo" />
     </StyledLink>
