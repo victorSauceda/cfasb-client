a8c91b521435c48c93357072a2b81a969c70e9cd
Victor <victorasauceda@gmail.com>
2020-10-22 10:46:17 -0700

    testing image

--- src/layouts/NavBar.jsx
+++ src/layouts/NavBar.jsx
@@ -30,7 +30,7 @@ const Nav = styled.nav`
 const NavBar = () => (
   <Headroom calcHeightOnResize disableInlineStyles>
     <StyledLink to="/">
-      <img style={{width:"4rem", height="auto"}} src={logo} alt="Santa Barbara Logo" />
+      <img style={{ width: '4rem' }} src={logo} alt="Santa Barbara Logo" />
     </StyledLink>
     <Nav>
       <Link to="/">Home</Link>
