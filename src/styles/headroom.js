7996e466505285bfc15c5aef63f20f3356d4e4c6
Victor <victorasauceda@gmail.com>
2020-10-22 17:28:27 -0700

    css

--- src/styles/headroom.js
+++ src/styles/headroom.js
@@ -21,37 +21,6 @@ const headroom = css`
       }
     }
   }
-  .headroom--unfixed {
-    position: relative;
-    transform: translateY(0);
-    transition: ${theme.transitions.headroom.transition};
-  }
-  .headroom--scrolled {
-    transition: ${theme.transitions.headroom.transition};
-  }
-  .headroom--unpinned {
-    position: fixed;
-    transform: translateY(-100%);
-    transition: ${theme.transitions.headroom.transition};
-  }
-  .headroom--pinned {
-    position: fixed;
-    transform: translateY(0);
-    transition: ${theme.transitions.headroom.transition};
-    background-color: ${theme.colors.white.light};
-    box-shadow: 0 5px 30px rgba(0, 0, 0, 0.2);
-    nav {
-      a {
-        color: ${theme.colors.black.base};
-        &:hover {
-          border-color: ${theme.colors.black.base};
-          color: ${theme.colors.black.base};
-        }
-        &:focus {
-          color: ${theme.colors.black.base};
-        }
-      }
-    }
     padding-top: 0.75rem;
     padding-bottom: 0.75rem;
     svg {
